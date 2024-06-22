bac9ad64ed21fee9671b144f11c7e2178a4dc5d8
sarath <sarathakash@gmail.com>
2024-06-22 18:10:20 +0530

    this is my new commit

--- script/build.sh
+++ script/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-sudo usermod -aG docker ubuntu
 sudo docker build -t ngimg .
-sudo docker run --name nginx-container -d -p 3000:80  ngimg
-sudo apt install docker-compose -y
-sudo docker-compose up -d
-sudo docker-compose build
 sudo docker tag ngimg sarathakashk95/prod:version-1
-sudo docker push sarathakashk95/prod:version-1
