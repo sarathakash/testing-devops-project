39f8f61b0f95527142e4bcfea1e37f26384be38b
sarath <sarathakash@gmail.com>
2024-06-23 20:53:29 +0530

    this is my sixth commit

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin docker.io
 docker push $DOCKER_BFLASK_IMAGE
-docker run -d -p 4040:80 $DOCKER_BFLASK_IMAGE
+docker run -d -p 8000:80 $DOCKER_BFLASK_IMAGE
