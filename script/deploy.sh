f5a2d0934522e95b0bde1355357eb5f3dd87b9f0
sarath <sarathakash@gmail.com>
2024-06-24 16:01:39 +0530

    this is the 200 commit

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin docker.io
 docker push $DOCKER_BFLASK_IMAGE
-docker run -d -p 8001:80 $DOCKER_BFLASK_IMAGE
+docker run -d -p 8002:80 $DOCKER_BFLASK_IMAGE
